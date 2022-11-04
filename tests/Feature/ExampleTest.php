9542568047e2320969adc883b33c5bb07cd464a6
Areeb Malik <areeb.web.intern@gmail.com>
2022-11-04 18:50:58 +0500

    Project setup and some key pointers

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Areeb\Mypackage\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
